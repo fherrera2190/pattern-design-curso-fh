0403f0c6d149506c7fa1c2115af2b3699124fcdd
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-07-10 21:46:13 -0300

    add file practice on js

---
+++ Files/main.js
@@ -0,0 +1,9 @@
+console.log("Hola")
+const inputFile = document.querySelector("input")
+const img = document.querySelector("img")
+
+inputFile.addEventListener("change", (event) => {
+    console.log(event.target.files)    
+    img.src = URL.createObjectURL(event.target.files[0])
+    console.log(img.src)
+})
