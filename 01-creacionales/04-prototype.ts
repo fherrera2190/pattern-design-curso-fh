8ba3669188ca8838ffe564fdbc9a0d882dfea914
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-01-31 16:27:44 -0300

    inmutabilidad

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,51 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+  public title: string;
+  private content: string;
+  public author: string;
+  public constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  displayInfo() {
+    console.log(`
+            Titulo: ${this.title}
+            Contenido: ${this.content}
+            Autor: ${this.author}
+            `);
+  }
+
+  clone() {
+    return new Document(this.title, this.content, this.author);
+  }
+}
+
+function main() {
+  const document1 = new Document("Cotizacion", "500 Dolares", "Fernando");
+  console.log({ document1 });
+  document1.displayInfo();
+
+  const document2 = { ...document1 };
+  document2.title = "Nueva cotizacion";
+  console.log({ document2 });
+  //document2.displayInfo(); Property 'displayInfo' does not exist on type '{ title: string; content: string; author: string; }'
+  document1.displayInfo();
+
+  //   const document2 = structuredClone(document1);
+  //   document2.title = "Nueva cotizacion";
+
+  //   console.log({ document2 });
+  //   document2.displayInfo();
+  //En ambos casos se pierde el adn "Document"
+
+//   const document2 = document1.clone();
+//   document2.title = "Nueva cotizacion";
+//   console.log({ document2 });
+//   document2.displayInfo();
+}
+main();
