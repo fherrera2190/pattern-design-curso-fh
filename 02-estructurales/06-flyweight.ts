86ef55d61c751224bfd861367a304c87463cbc48
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-02-12 18:15:22 -0300

    estructurales done!

--- 02-estructurales/06-flyweight.ts
+++ 02-estructurales/06-flyweight.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * ! Patrón Flyweight
  * Es un patrón de diseño estructural que nos permite usar objetos compartidos
@@ -8,3 +10,68 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+interface Location {
+  display(coordinates: { x: number; y: number }): void;
+}
+
+class LocationIcon implements Location {
+  private type: string;
+  private iconImage: string;
+
+  constructor(type: string, iconImage: string) {
+    this.type = type;
+    this.iconImage = iconImage;
+  }
+
+  display(coordinates: { x: number; y: number }): void {
+    console.log(
+      `Coords: ${coordinates.x}, ${coordinates.y} con icono ${this.iconImage}`,
+      COLORS.blue
+    );
+  }
+}
+
+// Fabrica
+
+class LocationFactory {
+  private icons: Record<string, LocationIcon> = {};
+
+  // Escuela , hospital, parque
+  getLocationIcon(type: string): LocationIcon {
+    if (!this.icons[type]) {
+      console.log(`Creando icono para ${type}`);
+      const iconImage = `imagen_de_${type.toLocaleLowerCase()}.png`;
+
+      this.icons[type] = new LocationIcon(type, iconImage);
+    }
+    return this.icons[type];
+  }
+}
+
+class MapLocation {
+  private coordinates: { x: number; y: number };
+  private icon: LocationIcon;
+
+  constructor(x: number, y: number, icon: LocationIcon) {
+    this.coordinates = { x, y };
+    this.icon = icon;
+  }
+
+  display() {
+    this.icon.display(this.coordinates);
+  }
+}
+
+function main() {
+  const factory = new LocationFactory();
+
+  const locations = [
+    new MapLocation(10, 20, factory.getLocationIcon("Escuela")),
+    new MapLocation(20, 40, factory.getLocationIcon("Escuela")),
+
+  ];
+
+  locations.forEach((location) => location.display());
+}
+
+main();
--- 02-estructurales/07-proxy.ts
+++ 02-estructurales/07-proxy.ts
@@ -10,3 +10,62 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Player {
+  name: string;
+  level: number;
+
+  constructor(name: string, level: number) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+interface Room {
+  enter(player: Player): void;
+}
+
+class SecretRoom implements Room {
+  enter(player: Player): void {
+    console.log(
+      `%cEl jugador ${player.name} ha entrado en la sala secreta.`,
+      COLORS.blue
+    );
+  }
+}
+
+// clase Proxy
+
+class MagicPortal implements Room {
+  private secretRoom: SecretRoom;
+
+  constructor(room: SecretRoom) {
+    this.secretRoom = room;
+  }
+
+  enter(player: Player): void {
+    if (player.level >= 10) {
+      this.secretRoom.enter(player);
+      return;
+    }
+    console.log(
+      `%cEl jugador ${player.name} no tiene suficiente nivel para entrar en la sala secreta.`,
+      COLORS.red
+    );
+  }
+}
+
+function main() {
+  const secretRoom = new SecretRoom();
+  const magicPortal = new MagicPortal(secretRoom);
+  const player1 = new Player("Juan", 15);
+  const player2 = new Player("Nemesis", 5);
+
+  magicPortal.enter(player1);
+
+  magicPortal.enter(player2);
+}
+
+main();
